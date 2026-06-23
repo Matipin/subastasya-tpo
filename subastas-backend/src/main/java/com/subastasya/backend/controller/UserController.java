991a90bf8638939416341b76d1fc99b5c791edde
matpin <matpina@uade.edu.ar>
2026-06-23 00:51:51 -0300

    Add Transactional to UserController to fix missing bids

--- subastas-backend/src/main/java/com/subastasya/backend/controller/UserController.java
+++ subastas-backend/src/main/java/com/subastasya/backend/controller/UserController.java
@@ -18,6 +18,7 @@ import java.time.LocalDateTime;
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
+@org.springframework.transaction.annotation.Transactional
 public class UserController {
 
     private final UsuarioRepository usuarioRepository;
